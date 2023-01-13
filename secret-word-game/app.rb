a7eec1c18c1d73131bba552755bf5aa1012accdf
fullstackraven <fullstackraven@gmail.com>
2023-01-13 06:37:21 -0500

    Fix formatting

--- secret-word-game/app.rb
+++ secret-word-game/app.rb
@@ -1,18 +1,17 @@
-#This is a playground project that I created myself based
-#on the freeCodeCamp Ruby tutorial secert word guessing game
+# This is a playground project that I created myself based
+# on the freeCodeCamp Ruby tutorial secert word guessing game
 
 file = File.open("secret-word.txt", "r")
 
 secret_word = file.read
 
-
-#Handles guess section variables
+# Handles guess section variables
 guess = ""
 guess_count = 0
 guess_limit = 5
 out_of_guesses = false
 
-#Handles hint section variables
+# Handles hint section variables
 hint = Array["It is a type of drink", "It makes a good car freshener", "You are out of hints"]
 hint_count = 0
 hint_limit = 2
@@ -21,16 +20,16 @@ out_of_hints = false
 puts "--- Welcome to the secret word guessing game! ---"
 puts "--- You must answer with the correct word to win ---"
 puts "--- *Be warned* ---
-            1. You only have 5 chances to win 
+            1. You only have 5 chances to win
             2. You may recieve hints by typing 'Hint or 'hint' as a guess"
 
-#Game Section
+# Game Section
 while guess != secret_word and !out_of_guesses
   if guess_count < guess_limit and guess_count != 4
     puts "Enter guess: "
     guess = gets.chomp()
     guess_count += 1
-    #Handles obtaining user input and keeps count of guesses
+    # Handles obtaining user input and keeps count of guesses
   elsif guess_count < guess_limit and guess_count == 4
     puts "Final guess:"
     guess = gets.chomp()
@@ -48,12 +47,12 @@ while guess != secret_word and !out_of_guesses
     out_of_hints = true
     puts hint[2]
   end
-  #Handles hint logic and outputs alerts to user based on count of hints
+  # Handles hint logic and outputs alerts to user based on count of hints
 end
 
 if out_of_guesses
-    puts "You lose"
+  puts "You lose"
 else
-    puts "You won!"
+  puts "You won!"
 end
-#Handles endgame alerts
+# Handles endgame alerts
