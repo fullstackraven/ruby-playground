713992462dd7218e59292b261048e5455e196f79
fullstackraven <fullstackraven@gmail.com>
2023-01-21 13:00:31 -0500

    Add display module and refactor gameplay calls

--- ravengard/lib/main.rb
+++ ravengard/lib/main.rb
@@ -4,18 +4,11 @@ require_relative 'warrior.rb'
 require_relative 'weapon.rb'
 require_relative 'armor.rb'
 require_relative 'intro.rb'
-
+require_relative 'display.rb'
 
 def play_game
   game = Intro.new
-  game.intro
-  game.update
-  game.identification
-  weapon_select = WeaponSelection.new
-  weapon_select.weapon
-  armor_select = ArmorSelection.new
-  armor_select.armor
-  game.decision1
+  game.play
   replay_game
 end
 
@@ -23,8 +16,8 @@ def replay_game
   puts "Would you like to play a new game?\n(a) Yes\n(b) No"
 
   puts "----------------"
-    sleep 1
-    puts "State your choice:"
+  sleep 1
+  puts "State your choice:"
   replay_input = gets.chomp.downcase
   if replay_input == 'a'
     play_game
