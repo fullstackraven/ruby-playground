5759b90f4d8da96f1d91d699fda4de3ae6548161
fullstackraven <fullstackraven@gmail.com>
2023-01-21 12:58:52 -0500

    Change mixins to include display

--- ravengard/lib/armor.rb
+++ ravengard/lib/armor.rb
@@ -1,9 +1,8 @@
-require_relative 'intro.rb'
+require_relative 'display.rb'
 
-class ArmorSelection < Intro
-  # include Player
+class ArmorSelection
+  include Display
   attr_accessor :armor
-  attr_reader :intro
 
   def initialize
     @armor = Array.new
@@ -26,7 +25,7 @@ class ArmorSelection < Intro
 
   def armor
     armor_options = "(a) Helmet\n(b) Chain mail\n(c) Round shield\n(d) Lamellar\n(e) Enchanted leather tunic\n(f) Cloth hooded cloak"
-    
+
     sleep 1
     puts " "
     puts "Choose your battle armor: "
@@ -38,33 +37,33 @@ class ArmorSelection < Intro
     answer = gets.chomp.downcase.to_s
     puts "----------------"
 
-      if answer == 'a'
-        answer = "Helmet"
-        @armor << answer
-        armor_new
-      elsif answer == 'b'
-        answer = "Chain mail"
-        @armor << answer
-        armor_new
-      elsif answer == "c"
-        answer = "Round shield"
-        @armor << answer
-        armor_new
-      elsif answer == "d"
-        answer = "Lamellar"
-        @armor << answer
-        armor_new
-      elsif answer == "e"
-        answer = "Enchanted leather tunic"
-        @armor << answer
-        armor_new
-      elsif answer == "f"
-        answer = "Cloth hooded cloak"
-        @armor << answer
-        armor_new
-      else
-        armor_error
-      end
+    if answer == 'a'
+      answer = "Helmet"
+      @armor << answer
+      armor_new
+    elsif answer == 'b'
+      answer = "Chain mail"
+      @armor << answer
+      armor_new
+    elsif answer == "c"
+      answer = "Round shield"
+      @armor << answer
+      armor_new
+    elsif answer == "d"
+      answer = "Lamellar"
+      @armor << answer
+      armor_new
+    elsif answer == "e"
+      answer = "Enchanted leather tunic"
+      @armor << answer
+      armor_new
+    elsif answer == "f"
+      answer = "Cloth hooded cloak"
+      @armor << answer
+      armor_new
+    else
+      armor_error
+    end
   end
 
   def armor_new
