38475e0c86deea3f3ed9e4c3355cedf383147717
fullstackraven <fullstackraven@gmail.com>
2023-01-25 23:28:18 -0500

    Create file and initial structure

---
+++ ravengard/lib/campaigns.rb
@@ -0,0 +1,74 @@
+require_relative 'display.rb'
+require_relative 'player.rb'
+
+class CampaignOne 
+  include Display
+  include Player
+
+  attr_accessor 
+
+  def initialize
+
+  end
+
+  def introduction
+    
+  end
+
+  def historian
+  end
+
+  def join_platoon
+  end
+
+  def engaging_enemy
+  end
+
+  def challenge1
+  end
+
+  def challenge2
+  end
+
+  def challenge3
+  end
+
+  def challenge4
+  end
+
+  def challenge5
+  end
+
+  def final_battle
+  end
+
+  def conclusion
+  end
+  
+end
+
+# Outline of Campaign 
+
+# Introduction: The player enters the battlefield as a new warrior, joining his platoon of fellow Vikings in the midst of a fierce battle. The player quickly learns that they are fighting against a powerful enemy clan that has been terrorizing the land of Ravengard for years.
+
+# Meeting the Historian Warrior: The player meets a seasoned warrior and historian, who tells them about the history of the enemy clan and the reasons behind the current conflict. The historian warrior also shares valuable information about the enemy's strengths and weaknesses, as well as strategies for how to defeat them.
+
+# Joining the Platoon: The player joins the platoon and quickly learns the ropes of battle. The player will be trained in the art of combat and will learn how to use different weapons and tactics. The player will also learn about the culture and traditions of the Ravengard people.
+
+# Engaging the Enemy: The player and the platoon engage the enemy clan in battle. The player will be faced with a series of challenging battles, each with their own unique obstacles and enemies. The player will have to use all of their training and skills to overcome these challenges.
+
+# The Final Battle: The player and the platoon finally face off against the leader of the enemy clan in a final, epic battle. The player will have to use all of their knowledge, skills, and strength to defeat the enemy leader and end the conflict once and for all.
+
+# Conclusion: The player and the platoon emerge victorious, and the enemy clan is defeated. The player is hailed as a hero for their bravery and skill in battle. The player will have the opportunity to reflect on the lessons learned and the sacrifices made during the campaign.
+
+# Outline of CampaignOne Challenges 
+
+# The first challenge the player will face is fighting in the mountainous terrain. The enemy clan has a stronghold in the mountains, and the player will have to navigate the steep and rocky terrain to reach them. To overcome this challenge, the player will need to use their knowledge of the terrain and their agility to navigate the mountains efficiently and safely.
+
+# The second challenge is a river crossing. The player will have to cross a fast-moving river to reach the enemy's stronghold. To overcome this challenge, the player will have to use their knowledge of the local rivers and their swimming skills to cross safely. They could also build a raft, or use a boat if available.
+
+# The third challenge is the enemy's use of traps and ambushes. The enemy clan has set traps and ambushes throughout the area to slow down and trap the player's platoon. To overcome this challenge, the player will have to be aware of their surroundings and watch for signs of traps and ambushes. They will also have to use their knowledge of the enemy's tactics to anticipate and avoid these traps.
+
+# The fourth challenge is the enemy's powerful archers. The enemy clan has skilled archers that can pick off the player and their platoon from a distance. To overcome this challenge, the player will have to use cover and terrain to protect themselves from the arrows, and use their own archery skills or other ranged weapons to take out the enemy archers.
+
+# The fifth challenge is the enemy's elite warriors. The enemy clan has a group of elite warriors that are highly skilled and dangerous in battle. To overcome this challenge, the player will have to use all of their combat skills and knowledge of the enemy's tactics to outsmart and defeat these elite warriors. They could also use their historical knowledge to find the weaknesses of the elite warriors and exploit them.
