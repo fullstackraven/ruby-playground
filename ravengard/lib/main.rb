5978d5c3f7553c5b57c567e5d79bb9cfd63f99ef
fullstackraven <fullstackraven@gmail.com>
2023-01-25 23:31:31 -0500

    Require new file and refactored for simplicity

--- ravengard/lib/main.rb
+++ ravengard/lib/main.rb
@@ -1,30 +1,17 @@
 require_relative 'history.rb'
 require_relative 'player.rb'
-require_relative 'warrior.rb'
 require_relative 'weapon.rb'
 require_relative 'armor.rb'
 require_relative 'intro.rb'
 require_relative 'display.rb'
+require_relative 'campaigns.rb'
 
 def play_game
-  game = Intro.new
-  game.play
-  replay_game
-end
-
-def replay_game
-  puts "Would you like to play a new game?\n(a) Yes\n(b) No"
-
-  puts "----------------"
-  sleep 1
-  puts "State your choice:"
-  replay_input = gets.chomp.downcase
-  if replay_input == 'a'
-    play_game
-  else
-    puts " "
-    puts "Thanks for playing Ravengard!"
-  end
+  include Display
+  intro = Intro.new
+  intro.play
+  display_replay_game
 end
 
 play_game
+
