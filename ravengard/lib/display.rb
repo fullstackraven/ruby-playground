c0ebae1cf7961357c0af9b376daa534b02a6abf0
fullstackraven <fullstackraven@gmail.com>
2023-01-25 23:28:53 -0500

    Fix layout of methods and add more functionality

--- ravengard/lib/display.rb
+++ ravengard/lib/display.rb
@@ -1,4 +1,5 @@
 module Display
+  # For intro only
   def display_intro_graphics
     puts " "
     puts "                           *-*-*-*-*-*"
@@ -33,32 +34,18 @@ module Display
   end
 
   def display_intro_name
-    puts "----------------"
     puts "Commander: Get up Warrior. Whats your name?"
     puts "----------------"
     sleep 1
     puts "State your name:"
   end
 
-  def display_intro_age(name, age)
+  def display_intro_age(name)
     puts "----------------"
     puts "Commander: Great #{name}, nice to meet you. Are you old enough for battle?"
     puts "----------------"
     sleep 1
     puts "State your age: "
-  end
-
-  def display_age_check(age)
-    loop do
-      if age < 18
-        puts display_input_warning
-        puts "State your age: "
-        age = gets.chomp().to_i
-      else
-        age >= 18
-      end
-      break if age >= 18
-    end
   end
 
   def display_intro_form
@@ -87,10 +74,89 @@ module Display
     puts "State your Warrior skill: "
   end
 
+  def display_state_info(string)
+    puts "----------------"
+    sleep 1
+    puts "State your #{string}:"
+  end
+
+  def display_update_id(level, name, age, form, location, skill)
+    display_state_info("name")
+    name = gets.chomp().capitalize
+    display_state_info("age")
+    age = gets.chomp().to_i
+    display_age_check
+    display_state_info("form")
+    form = gets.chomp().capitalize
+    display_state_info("location")
+    location = gets.chomp().capitalize
+    display_state_info("skill")
+    skill = gets.chomp().capitalize
+    display_identification(level, name, age, form, location, skill)
+  end
+
+  def display_update(level, name, age, form, location, skill)
+    puts " "
+    puts "Would you like to change any personal stats or information before moving on? You will not be able to edit after this point.\n(a) Yes\n(b) No"
+    display_choice
+    edit = gets.chomp().downcase
+    if edit == 'b'
+      display_save
+      display_identification(level, name, age, form, location, skill)
+    elsif edit == 'a'
+      display_update_id(level, name, age, form, location, skill)
+    end
+  end
+
+  def display_train_or_join(name, form, level)
+    loop do
+      puts " "
+      sleep 1
+      puts "Begin training for battle or join your legion platoon: "
+      puts "(a) Hone my skills\n(b) No practice. I'm ready for battle!"
+      puts " "
+      display_choice
+      run_campaign = gets.chomp().downcase.to_s
+      puts "----------------"
+
+      if run_campaign == 'a'
+        puts " "
+        sleep 1
+        puts "#{name}, a wise choice! Your training has leveled you up."
+        level += 1
+        display_level_up
+        player_level(level)
+        run_campaign = true
+      elsif run_campaign == 'b'
+        puts " "
+        puts "#{name}, all #{form}'s must be born ready. Your platoon is across the field. Collect your weapon and meet up with the platoon leader."
+        puts " "
+        run_campaign = true
+      else
+        puts display_input_warning
+      end
+      break if run_campaign == true
+    end
+  end
+
+  # Globally used methods
+  def display_age_check(age)
+    loop do
+      if age < 18
+        puts display_input_warning
+        puts "State your age: "
+        age = gets.chomp().to_i
+      else
+        age >= 18
+      end
+      break if age >= 18
+    end
+  end
+
   def display_battle_cry
     puts "----------------"
     sleep 1
-    puts "Commander: LETS GO TO BATTLE!"
+    puts "LETS GO TO BATTLE!"
   end
 
   def display_identification(level, name, age, form, location, skill)
@@ -104,44 +170,25 @@ module Display
     puts "Form: #{form}"
     puts "Location: #{location}"
     puts "Skills: #{skill}"
+    puts "Health: #{player_health}"
+    puts "Strength: #{player_strength}"
+    # weapon and armor output are displaying before the placeholder text
+    puts "Weapon: " + weapon_select.get_weapon.to_s
+    puts "Armor: " + armor_select.get_armor.to_s
     puts "*-*-*-*-*-*"
     puts " "
   end
 
-  def display_update_id(level, name, age, form, location, skill)
-    puts "----------------"
-    sleep 1
-    puts "State your name:"
-    name = gets.chomp().capitalize
-    puts "----------------"
-    sleep 1
-    puts "State your age:"
-    age = gets.chomp().to_i
-    loop do
-      age_check = false
-      if age < 18
-        puts "Error: Please input age over 18"
-      else
-        age_check = true
-      end
-      break if age_check == true
-    end
-    puts "----------------"
-    sleep 1
-    puts "State your form:"
-    form = gets.chomp().capitalize
-    puts "----------------"
-    sleep 1
-    puts "State your location:"
-    location = gets.chomp().capitalize
-    puts "----------------"
-    sleep 1
-    puts "State your skill:"
-    skill = gets.chomp().capitalize
+  def display_save
     puts "----------------"
     sleep 1
     puts "Saving Complete"
-    puts display_identification(level, name, age, form, location, skill)
+  end
+
+  def display_choice
+    puts "----------------"
+    sleep 1
+    puts "State your choice:"
   end
 
   def display_level_up
@@ -154,7 +201,27 @@ module Display
     puts " "
   end
 
+  def display_level_hint
+    puts "** Leveling up increases your warriors ability to inflict damage and heal **"
+  end
+
   def display_input_warning
     "\e[31mSorry, that is an invalid answer. Please, try again.\e[0m"
+  end
+
+  # For end of game only
+  def display_replay_game
+    puts "Would you like to play a new game?\n(a) Yes\n(b) No"
+  
+    puts "----------------"
+    sleep 1
+    puts "State your choice:"
+    replay_input = gets.chomp.downcase
+    if replay_input == 'a'
+      play_game
+    else
+      puts " "
+      puts "Thanks for playing Ravengard!"
+    end
   end
 end
