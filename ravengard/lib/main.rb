49fa1dea4f6b196aad1639587dbb0a35f751fef3
fullstackraven <fullstackraven@gmail.com>
2023-01-26 22:27:54 -0500

    Refactor gameplay structure

--- ravengard/lib/main.rb
+++ ravengard/lib/main.rb
@@ -9,7 +9,15 @@ require_relative 'campaigns.rb'
 def play_game
   include Display
   intro = Intro.new
-  intro.play
+  intro.play_intro
+  weapon_select = WeaponSelection.new
+  weapon_select.weapon
+  armor_select = ArmorSelection.new
+  armor_select.armor
+  intro.update_intro
+  armor_select.show_armor
+  weapon_select.show_weapon
+  intro.finish_intro
   display_replay_game
 end
 
